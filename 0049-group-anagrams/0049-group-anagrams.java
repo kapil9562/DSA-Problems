4422af346879fc04538a776ae32443c51049a021
Kapil Adhikari <adhikarikapil389@gmail.com>
2026-08-29 09:07:33 +0530

    Time: 6 ms (99.32%), Space: 49.6 MB (59.36%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -8,7 +8,11 @@ class Solution {
 
             String key = new String(chars);
 
-            map.computeIfAbsent(key, k -> new ArrayList<>()).add(str);
+            if(!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(str);
         }
 
         return new ArrayList<>(map.values());
